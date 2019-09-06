3ea52947abc0df48b16cc4afdf8cd8c22ebe3522
zuozelin <47731449+zuozelin@users.noreply.github.com>
2019-09-06 13:44:56 +0800

    190906

--- eight.py
+++ eight.py
@@ -1,32 +1,21 @@
-# 将小说的主要人物记录在文件中
+#列表推导式
+# 从1到10 所有偶数的平方
+alist = []
+for i in range(1,11):
+  if (i % 2 == 0):
+    alist.append( i *i )
 
-#file1 = open('name.txt' , 'w')
-#file1.write('诸葛亮')
-#file1.close()
 
-#file2 = open('name.txt')
-#print(file2.read())
-#file2.close()
+print(alist)
 
-#file3 = open('name.txt','a')
-#file3.write('刘备')
-#file3.close()
 
-#file4 = open('name.txt')
-#print(file4.readline())
+blist = [i*i for i in range(1, 11) if(i % 2) == 0]
 
-#file5 = open('name.txt')
-#for line in file5.readlines():
-#    print(line)
-#    print('=======')
+print(blist)
 
-file6 = open('name.txt')
-print('当前文件指针的位置 %s' %file6.tell())
-print('当前读取到了一个字符，字符的内容是  %s' %file6.read(1))
-#file.seek(5,0)   5：偏移位置（偏移量）   0：从文件开头偏移  1表示从当前位置偏移  2从文件结尾位置偏移
-file6.seek(0)
-print('我们进行了seek操作')
-print('当前文件指针的位置 %s' %file6.tell())
-print('当前读取到了一个字符，字符的内容是 %s' %file6.read(1))
-print('当前文件指针的位置 %s' %file6.tell())
-file6.close()
+#字典推导式
+z_num = {}
+for i in zodiac_name:
+  z_num[i] = 0
+
+z_num = {i:0 for i in zodiac_name}
